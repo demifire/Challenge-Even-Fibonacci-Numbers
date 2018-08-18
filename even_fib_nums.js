3a3622ed61ede1b0c56fa41f9ead037a4efa4772
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-08-18 13:29:57 -1000

    First commit

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -5,10 +5,23 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
-function _sumFibs( maxFibValue ) {
-  var sum = 0;
 
-  // do your work here
+ function _sumFibs( maxFibValue ) {
+  var sum = 0;
+  let arr = [1,2];
+
+    for(let i=1; i<=maxFibValue; i++){
+      let z = arr[i-1] + arr[i];
+      arr.push(z);
+    }
+    console.log(arr);
+
+    for(let j=0; arr[j]<=maxFibValue; j++){
+      if(arr[j]%2 === 0){
+        console.log("arr[j", arr[j])
+        sum += arr[j];
+      }
+    }
 
   return sum;
 }
@@ -18,9 +31,19 @@ function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
   //define your base case, validate your input
-
+  let arr = [1,2];
+  let i = 1;
+    while(arr[i]<=maxFibValue){
+      let z = arr[i-1] + arr[i];
+      arr.push(z);
+      i++;
+    }
+    console.log("while arr", arr);
 
   //do your work here
+
+  
+
 
   return highest;
 };
